20fc48ff64894f3c1f8fa8db6d8fe106999946bc
Philsun01 <philsun@gmail.com>
2019-09-11 20:26:07 -0700

    reading json file now

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const dbFunction = require('./db');
+const db = dbFunction('products.json');
 
 const app = express();
 
-app.get('/',(req, res, next )=>{
-  res.send("Hello World Again");
+app.get('/',async (req, res, next )=>{
+  const results = await db.findAll();
+  console.log(results);
+  res.send(results);
 });
 
 app.listen(3000, console.log('App function is listening') );
